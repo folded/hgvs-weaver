ee2b343f322cd4e472c625df19455485d1fe0b3b
Tobias Sargeant <tobias.sargeant@gmail.com>
2026-02-01 21:10:58 +1100

    Configure Read the Docs build environment.
    - Added Rust toolchain to .readthedocs.yaml.
    - Added maturin and project dependencies to docs/requirements.txt.
    - Updated conf.py with project metadata and logo configuration.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,11 +9,12 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../"))  # Adjust path to include the project root
+sys.path.insert(0, os.path.abspath("../../"))
 
 project = "weaver"
-copyright = "2025, Tobias Sargeant"
+copyright = "2026, Tobias Sargeant"
 author = "Tobias Sargeant"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,16 +22,28 @@ author = "Tobias Sargeant"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
     "sphinx_rtd_theme",
     "myst_parser",
 ]
 
 templates_path = ["_templates"]
-exclude_patterns: list[str] = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# Support both .rst and .md
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_logo = "_static/weaver.svg"
+html_theme_options = {
+    "logo_only": True,
+    "display_version": True,
+}
